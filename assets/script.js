293caaf02b5dd21581c1780491a34b094ac73ff5
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-04-25 20:03:03 -0700

    added state button and input to html.index and created variables on script.js

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var APIKEY = 'ac593f84fa47f530b0cc06150b019109';
+var searchBtn = document.getElementById('search-btn');
+var citySearch = document.getElementById('city-input');
+var stateSearch = document.getElementById('state-input');
+var weatherDetails = document.getElementById('weather-details');
+
+
