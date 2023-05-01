46135effde04888b9f9338869f071c7e28acfbfb
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-05-01 13:55:57 -0700

    debugged API key fetch request to meet proper subscription request, and added function to display results in html.index file

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
-var APIKEY = 'ac593f84fa47f530b0cc06150b019109';
-var searchBtn = document.getElementById('search-btn');
-var citySearch = document.getElementById('city-input');
-var stateSearch = document.getElementById('state-input');
-var weatherDetails = document.getElementById('weather-details');
-var searchHistory = document.getAnimations('search-history');
-var currentWeather = document.getElementById('current-weather');
-var forecastCards = document.getElementById('forecast-card');
-var searchForm = document.getElementById('search');
+const APIKEY = 'ac593f84fa47f530b0cc06150b019109';
+// var searchBtn = document.getElementById('search-btn');
+// var citySearch = document.getElementById('city-input');
+// var stateSearch = document.getElementById('state-input');
+// var weatherDetails = document.getElementById('weather-details');
+const searchHistory = document.getElementById('search-history');
+const currentWeather = document.getElementById('current-weather');
+const forecastCards = document.getElementById('forecast-card');
+const searchForm = document.getElementById('search');
 
 // function getLocation(event) {
 //     event.preventDefault();
@@ -26,23 +26,18 @@ var searchForm = document.getElementById('search');
 
 searchForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    if (!citySearch) return;
-    getWeather(citySearch)
-    addToSearchHistory(citySearch);
-});
-
-searchForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    if (!stateSearch) return;
-    getWeather(stateSearch)
-    addToSearchHistory(stateSearch);
+    const city = document.getElementById("city-input").value.trim();
+    if (!city) return;
+    getWeather(city);
+    addToSearchHistory(city);
+    console.log("Submitted:", city);
 });
 
 // event listener for search history
 searchHistory.addEventListener("click", (event) => {
-    var target = event.target;
+    const target = event.target;
     if (target.tagName === "BUTTON") {
-        var city = target.textContent.trim();
+        const city = target.textContent.trim();
         getWeather(city);
         console.log("search history:", city); //check if event listener works
     }
@@ -51,15 +46,15 @@ searchHistory.addEventListener("click", (event) => {
 
 // add city to search history
 function addToSearchHistory(city) {
-    var searchEl = document.createElement("button");
+    const searchEl = document.createElement("button");
     searchEl.textContent = city;
     searchEl.classList.add("search-item");
-    searchHistory.appendChild(searchItem);
+    searchHistory.appendChild(searchEl);
 }
 
 //fetch and display weather 
 function getWeather(city) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric')
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=imperial`)
     .then((response) => response.json())
     .then((data) => {
         console.log("current weather:", data); //check if fetching data
@@ -67,24 +62,47 @@ function getWeather(city) {
         return data.coord;
     })
     .then((coord) => {
-        fetch('https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&appid=${apiKey}&units=metric&exclude=current,minutely,hourly,alerts')
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${coord.lat}&lon=${coord.lon}&appid=${APIKEY}&units=imperial`) //forecast data
         .then((response) => response.json())
         .then((data) => {
-            displayForecast(data.daily);
+            console.log("Forecast data:", data); //check if forecast data comes up
+            displayForecast(data.list);
         });
     });
 }
 
 function displayCurrentWeather(data) {
-    var {name, date, weather, main, wind } = data;
-    var date = new Date(date * 1000).toLocaleDateString();
-    var icon = 'https://openweathermap.org/img/wn/${weather[0].icon}.png';
+    const { name, dt, weather, main, wind } = data;
+    const date = new Date(dt * 1000).toLocaleDateString();
+    const iconURL = `https://openweathermap.org/img/wn/${weather[0].icon}.png`;
 
     currentWeather.innerHTML = `
     <h2>${name} (${date})</h2>
-    <img src="${iconUrl}" alt="${weather[0].description}">
-    <p>Temperature: ${main.temp} °C</p>
+    <img src="${iconURL}" alt="${weather[0].description}">
+    <p>Temperature: ${main.temp} °F</p>
     <p>Humidity: ${main.humidity} %</p>
     <p>Wind Speed: ${wind.speed} m/s</p>
   `;
+}
+
+// to display 5day forecast info
+function displayForecast(forecastData) {
+    forecastCards.innerHTML = "";
+    const dailyForecasts = forecastData.filter((_, index) => index % 8 === 0);
+    
+  for (let i = 0; i < dailyForecasts.length; i++) {
+    const { dt_txt, weather, main, wind } = dailyForecasts[i];
+    const date = new Date(dt_txt).toLocaleDateString();
+    const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}.png`;
+    const forecastCard = document.createElement("div");
+    forecastCard.classList.add("forecast-card");
+    forecastCard.innerHTML = `
+      <h3>${date}</h3>
+      <img src="${iconUrl}" alt="${weather[0].description}">
+      <p>Temperature: ${main.temp} °F</p>
+      <p>Wind Speed: ${wind.speed} m/s</p>
+      <p>Humidity: ${main.humidity} %</p>
+    `;
+      forecastCards.appendChild(forecastCard);
+    }
 }
