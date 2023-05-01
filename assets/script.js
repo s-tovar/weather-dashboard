d672a67807fee3add442096bd8e4b40b890ac2ce
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-04-30 20:52:34 -0700

    modifed html.index by adding section where weather details will display

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,19 @@ var stateSearch = document.getElementById('state-input');
 var weatherDetails = document.getElementById('weather-details');
 
 
+function getLocation(event) {
+    event.preventDefault();
+    var requestURL = 'http:api.openweathermap.org/geo/1.0/direct?q=' + citySearch.value + ',' +
+    stateSearch.value + ',US&limit=5&appid=' + APIKEY;
+    fetch(requestURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(data) {
+            var lat = data[0].lat
+            var lon = data[0].lon
+            getWeather(lon, lat)
+        })
+        localStorage()
+}
+
